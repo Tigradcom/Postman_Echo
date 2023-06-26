3fcf0fe2a445b6e8e479aa5b283b8a3511278e2e
Tigradcom <tigradcom2@gmail.com@>
2023-06-27 00:04:39 +0400

    Test Passed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -13,7 +13,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("fata"))
+                .body("data", equalTo("some data"))
         ;
 
     }
