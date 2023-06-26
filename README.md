ec5b214b3f45e5da948e69a29182fc419bf3409b
Tigradcom <tigradcom2@gmail.com@>
2023-06-26 23:47:11 +0400

    Add files

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/vw6auvy57p66143r?svg=true)](https://ci.appveyor.com/project/Tigradcom/postman-echo)
---
+++ src/test/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnRest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+    }
+}
